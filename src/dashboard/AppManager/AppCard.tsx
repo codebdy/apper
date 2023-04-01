e728ac97bf85e818736426e81b1ce842dbafc5c3
Water.Li <lizongbin@hotmail.com>
2023-04-01 19:05:23 +0800

    Update AppCard.tsx

--- src/dashboard/AppManager/AppCard.tsx
+++ src/dashboard/AppManager/AppCard.tsx
@@ -1,8 +1,26 @@
-import { Card } from "antd"
+import { Card, Dropdown, MenuProps } from "antd"
 import { memo } from "react"
 import { EditOutlined, EllipsisOutlined, SettingOutlined, SendOutlined } from '@ant-design/icons';
 
 const { Meta } = Card;
+const items: MenuProps['items'] = [
+  {
+    label: <a href="https://www.antgroup.com">1st menu item</a>,
+    key: '0',
+  },
+  {
+    label: <a href="https://www.aliyun.com">2nd menu item</a>,
+    key: '1',
+  },
+  {
+    type: 'divider',
+  },
+  {
+    label: '3rd menu item',
+    key: '3',
+  },
+];
+
 export const AppCard = memo(() => {
   return (
     <Card
@@ -18,7 +36,7 @@ export const AppCard = memo(() => {
         <SendOutlined key="preview" />,
         <EditOutlined key="edit" />,
         <SettingOutlined key="setting" />,
-        <EllipsisOutlined key="ellipsis" />,
+        <Dropdown menu={{ items }} trigger={['click', 'hover']}><EllipsisOutlined key="ellipsis" /></Dropdown>,
       ]}
     >
       <Meta
