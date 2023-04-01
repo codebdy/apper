4c05d5c8143f10ec8043c2c4ee9e89ae40c14d34
Water.Li <lizongbin@hotmail.com>
2023-04-01 18:55:47 +0800

    Update AppCard.tsx

--- src/dashboard/AppManager/AppCard.tsx
+++ src/dashboard/AppManager/AppCard.tsx
@@ -1,15 +1,27 @@
-import { Card } from "antd"
+import { Avatar, Card } from "antd"
 import { memo } from "react"
+import { EditOutlined,EllipsisOutlined,SettingOutlined } from '@ant-design/icons';
 
 const { Meta } = Card;
 export const AppCard = memo(() => {
   return (
     <Card
-      hoverable
       style={{ width: "100%" }}
-      cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+      cover={
+        <img
+          alt="example"
+          src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+        />
+      }
+      actions={[
+        <SettingOutlined key="setting" />,
+        <EditOutlined key="edit" />,
+        <EllipsisOutlined key="ellipsis" />,
+      ]}
     >
-      <Meta title="Europe Street beat" description="www.instagram.com" />
+      <Meta
+        title="Card title"
+      />
     </Card>
   )
 })
