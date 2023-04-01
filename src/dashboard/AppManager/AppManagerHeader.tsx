2859f50afeee42c3dc5b712c6d356ed57ea1dc3b
Water.Li <lizongbin@hotmail.com>
2023-04-01 18:27:52 +0800

    Update AppManagerHeader.tsx

--- src/dashboard/AppManager/AppManagerHeader.tsx
+++ src/dashboard/AppManager/AppManagerHeader.tsx
@@ -1,10 +1,11 @@
-import { Input } from "antd";
+import { Button, Input, Space } from "antd";
 import { Spring } from "../Spring";
 import { memo } from "react"
 import styled from "styled-components"
+import { PlusOutlined, ShoppingOutlined } from "@ant-design/icons"
 
 const Container = styled.div`
-  height: 56px;
+  height: 64px;
   display: flex;
   align-items: center;
 `
@@ -15,6 +16,14 @@ export const AppManagerHeader = memo(() => {
     <Container>
       <Search placeholder="input search text" style={{ width: 300 }} />
       <Spring />
+      <Space>
+        <Button type="primary" icon={<PlusOutlined />}>
+          导入应用
+        </Button>
+        <Button type="primary" danger disabled icon={<ShoppingOutlined />}>
+          应用市场
+        </Button>
+      </Space>
     </Container>
   )
 })
