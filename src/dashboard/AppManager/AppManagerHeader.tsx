23208ec1577b5d16073b3a5ce56d3ff0e0ec9ebe
Water.Li <lizongbin@hotmail.com>
2023-04-01 18:19:16 +0800

    Update AppManagerHeader.tsx

--- src/dashboard/AppManager/AppManagerHeader.tsx
+++ src/dashboard/AppManager/AppManagerHeader.tsx
@@ -1,15 +1,20 @@
+import { Input } from "antd";
+import { Spring } from "../Spring";
 import { memo } from "react"
 import styled from "styled-components"
 
 const Container = styled.div`
   height: 56px;
   display: flex;
+  align-items: center;
 `
+const { Search } = Input;
 
-export const AppManagerHeader = memo(()=>{
-  return(
+export const AppManagerHeader = memo(() => {
+  return (
     <Container>
-
+      <Search placeholder="input search text" style={{ width: 300 }} />
+      <Spring />
     </Container>
   )
 })
