08e313678844c9706e0eb6bb628cdb84f704aede
Water.Li <lizongbin@hotmail.com>
2023-04-08 19:57:10 +0800

    backup

--- src/common/ModelBoard/ModelToolbar/index.tsx
+++ src/common/ModelBoard/ModelToolbar/index.tsx
@@ -1,5 +1,27 @@
 import React, { memo } from "react";
-import "./style.less";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 48px;
+  background-color: ${props => props.theme.token?.colorBgBase};
+  align-items: center;
+  border-bottom: solid 1px ${props => props.theme.token?.colorBorder};
+  .toolbarInner {
+    flex: 1;
+    display: flex;
+    height: 100%;
+    margin-right: 16px;
+    margin-left: 16px;
+    align-items: center;
+  };
+  .saveButtonShell {
+    display: flex;
+    align-items: center;
+    margin-left: 32px;
+  };
+`
 
 export const ModelToolbar = memo((
   props: {
@@ -8,10 +30,10 @@ export const ModelToolbar = memo((
 ) => {
   const { children } = props;
   return (
-    <div className={"model-toolbar"}>
+    <Container className={"model-toolbar"}>
       <div className={"toolbarInner"}>
         {children}
       </div>
-    </div >
+    </Container >
   );
 });
