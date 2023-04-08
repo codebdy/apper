c0afc0422e20194875f8da25e051644535354281
Water.Li <lizongbin@hotmail.com>
2023-04-08 17:19:57 +0800

    Update atoms.ts

--- src/recoil/atoms.ts
+++ src/recoil/atoms.ts
@@ -1,10 +1,5 @@
 import { atom } from "recoil";
 
-export const appsLoadingState = atom<boolean>({
-  key: "appsLoading",
-  default: false,
-})
-
 export const themeModeState = atom<'light' | 'dark'>({
   key: "themeMode",
   default: 'dark',
