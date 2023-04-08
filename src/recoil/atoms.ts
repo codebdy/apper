893916599ab20d0cb841686f09593d351a51d0ae
Water.Li <lizongbin@hotmail.com>
2023-04-08 17:17:15 +0800

    Update atoms.ts

--- src/recoil/atoms.ts
+++ src/recoil/atoms.ts
@@ -1,10 +1,4 @@
 import { atom } from "recoil";
-import { IApp } from "../model";
-
-export const appsState = atom<IApp[]>({
-  key: "apps",
-  default: [],
-})
 
 export const appsLoadingState = atom<boolean>({
   key: "appsLoading",
