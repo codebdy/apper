9b22e921c5d239e0a33d31d5f330ef2115f1e5a3
Water.Li <lizongbin@hotmail.com>
2023-04-08 17:15:17 +0800

    backup

--- src/ServiceDesigner/ServiceDesignerHeader/index.tsx
+++ src/ServiceDesigner/ServiceDesignerHeader/index.tsx
@@ -9,12 +9,14 @@ import styled from "styled-components"
 import { useToken } from "antd/es/theme/internal"
 import AvatarMenu from "components/AvatarMenu"
 import SelectLang from "components/LangSelect"
+import { ThemeSwitchButton } from "common/ThemeSwitchButton"
 
 const StyledHeader = styled.div`
   display: flex;
   align-items: center;
   padding: 0px 16px;
   border-bottom: solid 1px ${props => props.theme.token?.colorBorder};
+  height: 56px;
 `
 
 export const ServiceDesignerHeader = memo((props: {
@@ -31,17 +33,16 @@ export const ServiceDesignerHeader = memo((props: {
 
   return (
     <StyledHeader style={{ backgroundColor: token.colorBgContainer }}>
-      <Button type="text" shape="circle" size="large" onClick={handleBack}><HomeOutlined /></Button>
+      <Button type="text" icon={<HomeOutlined />} onClick={handleBack}></Button>
       <Divider type='vertical' />
       <div className="app-title" style={{ marginLeft: "4px" }}>服务名称</div>
       <div>Button</div>
       <div style={{ flex: 1 }}></div>
       <Space>
-        <Button type="text" size='large' shape="circle" icon={<QuestionCircleOutlined />} />
+        <ThemeSwitchButton />
+        <Button type="text" icon={<QuestionCircleOutlined />} />
         <Button
           type="text"
-          shape="circle"
-          size='large'
           icon={<GithubOutlined />}
           href="https://github.com/rxdrag/apper"
           target="_blank"
--- src/Studio/index.tsx
+++ src/Studio/index.tsx
@@ -5,12 +5,11 @@ import { Badge, Button, Divider, Space } from "antd"
 import { AppstoreOutlined, CloudServerOutlined, SettingOutlined, BellOutlined } from "@ant-design/icons"
 import { Spring } from "./Spring"
 import { useTranslation } from "react-i18next"
-import { useRecoilState } from "recoil"
-import { themeModeState } from "recoil/atoms"
 import AvatarMenu from "components/AvatarMenu"
 import LangSelect from "components/LangSelect"
 import { Outlet, useMatch, useNavigate } from "react-router-dom"
 import { DashboardRoutes } from "./Routes"
+import { ThemeSwitchButton } from "common/ThemeSwitchButton"
 
 const Container = styled.div`
   width: 100%;
@@ -46,14 +45,10 @@ const Content = styled.div`
 
 export const Studio = memo(() => {
   const { t } = useTranslation();
-  const [themeMode, setThemeMode] = useRecoilState(themeModeState)
+
   const navigate = useNavigate();
 
   const match = useMatch(`/*`)
-
-  const handleToggleTheme = useCallback(() => {
-    setThemeMode(mode => mode === 'light' ? 'dark' : 'light')
-  }, [setThemeMode])
 
   const handleToAppManager = useCallback(() => {
     navigate(`/${DashboardRoutes.AppManager}`)
@@ -89,13 +84,7 @@ export const Studio = memo(() => {
         </Space>
         <Spring />
         <Space>
-          <Button type="text" icon={
-            themeMode === 'light' ?
-              <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 24 24" width="20px" fill="currentColor"><rect fill="none" height="24" width="24" /><path d="M9.37,5.51C9.19,6.15,9.1,6.82,9.1,7.5c0,4.08,3.32,7.4,7.4,7.4c0.68,0,1.35-0.09,1.99-0.27C17.45,17.19,14.93,19,12,19 c-3.86,0-7-3.14-7-7C5,9.07,6.81,6.55,9.37,5.51z M12,3c-4.97,0-9,4.03-9,9s4.03,9,9,9s9-4.03,9-9c0-0.46-0.04-0.92-0.1-1.36 c-0.98,1.37-2.58,2.26-4.4,2.26c-2.98,0-5.4-2.42-5.4-5.4c0-1.81,0.89-3.42,2.26-4.4C12.92,3.04,12.46,3,12,3L12,3z" /></svg>
-              : <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 24 24" width="20px" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M6.76 4.84l-1.8-1.79-1.41 1.41 1.79 1.79zM1 10.5h3v2H1zM11 .55h2V3.5h-2zm8.04 2.495l1.408 1.407-1.79 1.79-1.407-1.408zm-1.8 15.115l1.79 1.8 1.41-1.41-1.8-1.79zM20 10.5h3v2h-3zm-8-5c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6zm0 10c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm-1 4h2v2.95h-2zm-7.45-.96l1.41 1.41 1.79-1.8-1.41-1.41z" /></svg>
-          }
-            onClick={handleToggleTheme}
-          />
+          <ThemeSwitchButton />
           <Badge count={5} offset={[-6, 2]}>
             <Button type="text" icon={<BellOutlined />} />
           </Badge>
