880d489f2bdcd0fc27734f680de003c5f233ccad
Water.Li <lizongbin@hotmail.com>
2023-04-15 16:47:21 +0800

    backup

--- src/ServiceDesigner/ServiceModel/index.tsx
+++ src/ServiceDesigner/ServiceModel/index.tsx
@@ -1,8 +1,12 @@
 import { UmlEditor } from "UmlEditor"
 import { memo } from "react"
+import SaveActions from "./SaveActions"
 
 export const ServiceModel = memo(() => {
   return (
-    <UmlEditor options={{ supportCustomizedApi: true }} />
+    <UmlEditor
+      options={{ supportCustomizedApi: true }}
+      actions={<SaveActions metaId="" />}
+    />
   )
 })
