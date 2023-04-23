9697f9e9aab0e44649343c857b38ac6b5493fdaf
Water.Li <lizongbin@hotmail.com>
2023-04-23 19:30:29 +0800

    Update GraphLogicAction.tsx

--- src/UmlEditor/EntityTree/GraphLogicLabel/GraphLogicAction.tsx
+++ src/UmlEditor/EntityTree/GraphLogicLabel/GraphLogicAction.tsx
@@ -1,5 +1,5 @@
 import { DeleteOutlined } from "@ant-design/icons";
-import { useDeleteScriptLogic } from "UmlEditor/hooks/useDeleteScriptLogic";
+import { useDeleteGraphLogic } from "UmlEditor/hooks/useDeleteGraphLogic";
 import { useMetaId } from "UmlEditor/hooks/useMetaId";
 import { MethodMeta } from "UmlEditor/meta";
 import { Button } from "antd";
@@ -12,7 +12,7 @@ export const GraphLogicAction = memo((
 ) => {
   const { graphLogic } = props;
   const metaId = useMetaId();
-  const deleteOrches = useDeleteScriptLogic(metaId)
+  const deleteOrches = useDeleteGraphLogic(metaId)
 
   const handleDelete = useCallback(() => {
     deleteOrches(graphLogic.uuid)
