29c46a12c1f3ba5a6f9be437443d8f5ad8816721
Water.Li <lizongbin@hotmail.com>
2023-07-05 09:33:24 +0800

    Update index.tsx

--- src/UmlEditor/LogicEditor/index.tsx
+++ src/UmlEditor/LogicEditor/index.tsx
@@ -1,6 +1,6 @@
 import { EditOutlined } from "@ant-design/icons"
-import { Button } from "antd"
-import { memo } from "react"
+import { Button, Modal } from "antd"
+import { memo, useCallback, useState } from "react"
 import { useTranslation } from "react-i18next"
 import styled from "styled-components"
 
@@ -11,13 +11,50 @@ const Container = styled.div`
   align-items: center;
 `
 
+const EditorShell = styled.div`
+  display: flex;
+  height: calc(100vh - 200px);
+`
+
 export const LogicEditor = memo(() => {
+  const [open, setOpen] = useState(false);
   const { t } = useTranslation();
+
+  const handleEditClick = useCallback(() => {
+    setOpen(true)
+  }, [])
+
+  const handleClose = useCallback(() => {
+    setOpen(false)
+  }, [])
+
   return (
     <Container>
-      <Button type="primary" size="large" icon={<EditOutlined />}>
+      <Button
+        type="primary"
+        size="large"
+        icon={<EditOutlined />}
+        onClick={handleEditClick}
+      >
         {t("UmlEditor.EditLogic")}
       </Button>
+      <Modal
+        title={t("UmlEditor.EditLogic")}
+        centered
+        open={open}
+        maskClosable={false}
+        onOk={handleClose}
+        onCancel={handleClose}
+        width={"calc(100vw - 100px)"}
+        okText={t("Confirm")}
+        cancelText={t("Cancel")}
+      >
+        <EditorShell>
+          <p>some contents...</p>
+          <p>some contents...</p>
+          <p>some contents...</p>
+        </EditorShell>
+      </Modal>
     </Container>
   )
 })
