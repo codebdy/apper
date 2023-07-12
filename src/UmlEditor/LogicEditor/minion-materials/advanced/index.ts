7b87ed0c922f4265b03454023c6543bbd5467370
Water.Li <lizongbin@hotmail.com>
2023-07-12 17:35:31 +0800

    backup

--- src/UmlEditor/LogicEditor/minion-materials/advanced/index.ts
+++ src/UmlEditor/LogicEditor/minion-materials/advanced/index.ts
@@ -1,8 +1,10 @@
 import { IActivityMaterial } from "@rxdrag/minions-schema";
 import { subLogicFlowMaterial } from "./subLogicFlow";
 import { customizedLoopMaterial } from "./customizedLoop";
+import { transactionMaterial } from "./transaction";
 
 export const advancedActivities: IActivityMaterial<any, any, any, any>[] = [
   customizedLoopMaterial,
+  transactionMaterial,
   subLogicFlowMaterial,
 ]
